3d911553d34dd9881cd0910580ac42b3205aa3b2
Yuting Hsiao <and535014@gmail.com>
2022-08-18 13:08:47 +0800

    安裝 dotenv module

--- store/index.js
+++ store/index.js
@@ -1,3 +1,6 @@
+const token = process.env.BEARER_TOKEN;
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+
 export const state = () => ({
     tweets: []
 });
@@ -14,9 +17,25 @@ export const mutations = {
 
 export const actions = {
     async fetchTweets({ commit }){
-        const json = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then((res)=> res.json())
+        // const json = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        // .then((res)=> res.json())
 
+        const params = {
+            'query': 'from:twitterdev -is:retweet',
+            'tweet.fields': 'author_id'
+        }
+
+        const json = await this.$axios.get(endpointUrl, {
+            params: params,
+            headers: {
+                "User-Agent": "my Twitter App",
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then((res)=> res)
+
+
+        // console.log(token);
         commit('setTweets', json)
     }
 };
