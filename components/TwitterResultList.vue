5789a084381f440041e9b6dac17319352a2f91f4
Yuting Hsiao <and535014@gmail.com>
2022-08-19 01:52:41 +0800

    新增滾動到底自動載入的功能

--- components/TwitterResultList.vue
+++ components/TwitterResultList.vue
@@ -6,7 +6,7 @@
         template(v-else)
             .not-found
                 p {{ msg }}
-    .list__btn(v-if="tweets.length")
+    //- .list__btn(v-if="tweets.length")
         button.btn.btn--primary(@click="fetchNextTweets") 更多
 </template>
 
@@ -15,11 +15,34 @@ import { mapActions } from "vuex";
 import { mapState } from "vuex";
 export default {
     name: 'TwitterResultList',
+    data(){
+        return {
+            scrollY: null
+        }
+    },
     computed: {
         ...mapState(['tweets','msg', 'resultCount', 'keywords', 'data', 'users']),
     },
     methods: {
-        ...mapActions(['fetchNextTweets'])
+        ...mapActions(['fetchNextTweets']),
+        handleScroll(){
+            let windowScrollY = window.scrollY
+            this.scrollY = windowScrollY
+            let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+            let windowHeight = document.documentElement.clientHeight || document.body.clientHeight
+            let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+            let delta = scrollTop+windowHeight - scrollHeight
+            // console.log(scrollTop, windowHeight, scrollHeight, scrollTop+windowHeight, delta);
+            if( delta > -10 ){
+                this.fetchNextTweets()
+            }
+        }
+    },
+    created(){
+        if(process.browser){
+            window.addEventListener('scroll', this.handleScroll)
+        }
+
     }
 }
 </script>
@@ -31,6 +54,7 @@ export default {
     padding: $result-list-inner-p;
     border-left: 1px solid $border-color;
     box-sizing: border-box;
+    overflow: auto;
     .list__content{
         margin-bottom: $result-list-inner-mb;
         .card{
